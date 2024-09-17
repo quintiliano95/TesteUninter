c8b5484dd0a171f78453bb3ef14d0a6430460e31
Alexandre Quintiliano <98800085+quintiliano95@users.noreply.github.com>
2024-09-17 12:38:39 -0300

    .

--- app.py
+++ app.py
@@ -10,15 +10,14 @@ NASA_API_URL = "https://images-assets.nasa.gov/recent.json"
 def fetch_albums():
     response = requests.get(NASA_API_URL)
     data = response.json()
-    return data['collection']['items'][:100]  # Limitar aos primeiros 100 álbuns
+    return data['collection']['items'][:100]
 
 
 @app.route('/')
 def home():
     albums = fetch_albums()
-    selected_albums = random.sample(albums, 16)  # Seleciona 16 álbuns aleatórios
+    selected_albums = random.sample(albums, 16)
 
-    # Cria uma lista formatada com os dados relevantes para a página inicial
     albums_data = []
     for album in selected_albums:
         title = album['data'][0]['title']
@@ -26,9 +25,8 @@ def home():
         preview_image = album['links'][0]['href'] if 'links' in album else None
         album_id = album['data'][0]['nasa_id']
 
-        # Formata a data para o padrão brasileiro (dd/mm/aaaa)
-        date_created = date_created.split("T")[0]  # Mantém apenas a parte da data
-        date_created = "-".join(reversed(date_created.split("-")))  # Converte para dd-mm-yyyy
+        date_created = date_created.split("T")[0]
+        date_created = "-".join(reversed(date_created.split("-")))
 
         albums_data.append({
             'title': title,
